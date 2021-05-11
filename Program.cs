f4c0c6a8c56a9d1f604d519b806fb6ae3d5c1d40
unknown <ssgsj.carranza@gmail.com>
2021-05-11 11:58:00 -0500

    inital

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPSLScs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
